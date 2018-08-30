d682f25fbf748679583605d4bbe93e9dfdc40d91
christriants <christopher.triantafilis@livestream.com>
2018-08-29 23:19:44 -0400

    Started project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /*
   Please add all Javascript code to this file.
 */
+
+const fetch = require('node-fetch')
+const auth = require('./auth.js')
+
+const token = auth.apiToken
+const clientId = auth.clientId
+const timestamp = auth.timestamp
+
+async function getUpcomingEvents() {
+  const getReq = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const upcomingEvents = `https://livestreamapis.com/v3/accounts/12963240/upcoming_events?clientId=${clientId}&token=${token}&timestamp=${timestamp}`
+  const res = await fetch(upcomingEvents, getReq);
+  const json = await res.json()
+  console.log(json)
+}
+
+getUpcomingEvents()
---
+++ js/auth.js
@@ -0,0 +1,25 @@
+var crypto = require('crypto');
+
+const API_SECRET = "HTuRddLgDjCZcnJdo0kvpYY9HOX9RCFA";
+const clientId = 8143;
+var timestamp = (new Date).getTime();
+
+function generateToken(scope, timestamp) {
+    var hmac = crypto.createHmac("md5", API_SECRET);
+    var tokenString = API_SECRET + ":" + scope + ":" + timestamp
+    hmac.update(tokenString);
+    var crypted = hmac.digest("hex");
+
+    return crypted;
+};
+
+var apiToken = generateToken('readonly', timestamp);
+
+// console.log(apiToken)
+
+module.exports = {
+     clientId: clientId,
+     timestamp: timestamp,
+     apiToken: apiToken,
+     API_SECRET: API_SECRET
+};
